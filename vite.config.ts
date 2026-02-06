3fbeb84aff80d7046f5f3d975407af951990f1ac
Rajvi <rajvigupta04@gmail.com>
2026-02-06 13:36:10 +0530

    test deployment ready

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   server: {
+    allowedHosts: [
+      'controlless-garnet-centrobaric.ngrok-free.dev'
+    ],
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
